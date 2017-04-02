b16c4a9c4cf2817c3b69d92d047de240d4606f78
Mator <why101@msn.com>
2017-04-02 16:04:20 -0700

    menger sponge is now centered on 0,0,0, set default menger sponge to iteration 9

--- menger/init.lua
+++ menger/init.lua
@@ -1,13 +1,20 @@
 -- Parameters
 
 local YWATER = -31000
-local menger_size = 729
+local menger_iteration = 9 -- min value 0, max value 10
 local DEBUG = true
 local menger_block = minetest.get_content_id("default:stone")
 
 -- Set mapgen parameters
 
+local menger_size = math.pow(3, menger_iteration)
+local menger_origin = math.floor(0 - menger_size / 2)
 minetest.set_mapgen_params({mgname = "singlenode", flags = "nolight", water_level = YWATER})
+
+if DEBUG then
+  print ("[menger] origin: "..menger_origin)
+  print ("[menger] size: "..menger_size)
+end
 
 -- Localise data buffer
 
@@ -21,9 +28,9 @@ end
 
 -- Helper function, tests if a point is outside of the object region
 
-function outside_region(d, minp, maxp)
-  return (maxp.x < 0) or (maxp.y < 0) or (maxp.z < 0) 
-      or (minp.x > d) or (minp.y > d) or (minp.z > d)
+function outside_region(s, d, minp, maxp)
+  return (maxp.x < s) or (maxp.y < s) or (maxp.z < s) 
+      or (minp.x > s + d) or (minp.y > s + d) or (minp.z > s + d)
 end
 
 -- Helper function, tests if a point is in the Menger Sponge
@@ -57,28 +64,28 @@ minetest.register_on_generated(function(minp, maxp, seed)
   local area = VoxelArea:new{MinEdge = emin, MaxEdge = emax}
   local data = vm:get_data(dbuf)
 
-  if outside_region(menger_size, minp, maxp) then
+  if outside_region(menger_origin, menger_size, minp, maxp) then
     if DEBUG then
-      print("Skipping "..region_text(minp, maxp))
+      print("[menger] Skipping "..region_text(minp, maxp))
     end
   else
     if DEBUG then
-      print ("Generating blocks in "..region_text(minp, maxp))
+      print ("[menger] Generating blocks in "..region_text(minp, maxp))
     end
     
     -- Iterate over fixed region for the menger sponge
-    local x1 = math.min(maxp.x, menger_size)
-    local y1 = math.min(maxp.y, menger_size)
-    local z1 = math.min(maxp.z, menger_size)
-    local x0 = math.max(minp.x, 0)
-    local y0 = math.max(minp.y, 0)
-    local z0 = math.max(minp.z, 0)
+    local x1 = math.min(maxp.x, menger_origin + menger_size)
+    local y1 = math.min(maxp.y, menger_origin + menger_size)
+    local z1 = math.min(maxp.z, menger_origin + menger_size)
+    local x0 = math.max(minp.x, menger_origin)
+    local y0 = math.max(minp.y, menger_origin)
+    local z0 = math.max(minp.z, menger_origin)
 
     for z = z0, z1 do
       for y = y0, y1 do
         local vi = area:index(x0, y, z)
         for x = x0, x1 do
-          if menger_test(menger_size, x, y, z) then
+          if menger_test(menger_size, x - menger_origin, y - menger_origin, z - menger_origin) then
             data[vi] = menger_block
           end
           vi = vi + 1
@@ -99,6 +106,6 @@ minetest.register_on_generated(function(minp, maxp, seed)
 end)
   
 minetest.register_on_newplayer(function(player)
-  local elevation = menger_size + 1
-  player:setpos({x=0, y=elevation, z=0})
+  local elevation = menger_origin + menger_size
+  player:setpos({x=menger_origin, y=elevation, z=menger_origin})
 end)
